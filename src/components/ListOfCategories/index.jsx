c09228fc7dca3c4029b08b0931f4a84c4480aed0
Luis Vazquez <luvazpa@gmail.com>
2021-06-29 18:20:02 -0500

    :sparkles: Intersection Observer
    Support for al browser for intersection observer added

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Category from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../db.json'
 
 const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+  useEffect(() => {
+    window
+      .fetch('https://petgram-server-devguicho.vercel.app/categories')
+      .then((res) => res.json())
+      .then((response) => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed && 'fixed'}>
       {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+  return (
+    <>
+      {renderList()} {showFixed && renderList(true)}
+    </>
   )
 }
 
--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { MdFavoriteBorder } from 'react-icons/md'
-import { ImgWrapper, Img, Button } from './styles'
+import { ImgWrapper, Img, Button, Article } from './styles'
 
 const DEFAULT_IMAGE =
   'https://images.unsplash.com/photo-1500879747858-bb1845b61beb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 const PhotoCard = ({ id, src = DEFAULT_IMAGE, likes = 0 }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          console.log('si')
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
+    })
+  }, [element])
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={element}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   )
 }
 
