b42d936018cd5f5af09e89cee147dea48bf3bc3a
Luis Vazquez <luvazpa@gmail.com>
2021-07-02 13:47:03 -0500

    :sparkles: PWA support added
    PWA support added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+/* eslint-disable prefer-regex-literals */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const { GenerateSW } = require('workbox-webpack-plugin')
 module.exports = {
   output: {
     filename: 'app.bundle.js',
@@ -39,6 +42,55 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css'
+    }),
+    new WebpackPwaManifest({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      short_name: 'Petgram 🐶',
+      description:
+        'Con petgram puedes encontrar fotos de animales domésticos muy facilmente',
+      orientation: 'portrait',
+      display: 'standalone',
+      start_url: '/home',
+      scope: '/',
+      background_color: '#fff',
+      theme_color: '#FC5500',
+      icons: [
+        {
+          src: path.resolve(__dirname, 'src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('Icons'),
+          ios: true
+        },
+        {
+          src: path.resolve('src/assets/maskable_icon.png'),
+          size: '1142x1142',
+          purpose: 'maskable'
+        }
+      ]
+    }),
+    new GenerateSW({
+      swDest: 'service-worker.js',
+      clientsClaim: true,
+      skipWaiting: true,
+      maximumFileSizeToCacheInBytes: 5000000,
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            'https://(res.cloudinary.com|images.unsplash.com)'
+          ),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-devguicho.vercel.app'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   devServer: {
